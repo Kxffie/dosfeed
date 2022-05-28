791ff65485ecf22222df4a2f68c69bddeb552fa2
= <bryceriordan2005@gmail.com>
2022-05-27 23:51:39 -0500

    c

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.scss'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+    )
+}
+
+export default MyApp
